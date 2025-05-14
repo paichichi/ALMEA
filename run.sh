e9de766ae9baa92b6de6dd1087cfa58e0d1d3d46
paichichi <964466242@qq.com>
2025-05-14 23:09:23 +0800

    123123

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ bash run_experiments.sh 0 FBDB15K 0.2 0.45
 #bash run_experiments.sh 0 FBDB15K 0.5 0.45
 #bash run_experiments.sh 0 FBDB15K 0.8 0.45
 
-#echo "Starting FBYG15K experiments"
+echo "Starting FBYG15K experiments"
 #bash run_experiments.sh 0 FBYG15K 0.2 0.50
 #bash run_experiments.sh 0 FBYG15K 0.5 0.50
 #bash run_experiments.sh 0 FBYG15K 0.8 0.50
